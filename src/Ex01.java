f2a51fb3aab897d347ac330b9fcb835e9bada585
Hong <h6752153@naver.com>
2024-04-17 16:15:51 +0900

    set commit

--- src/Ex01.java
+++ src/Ex01.java
@@ -2,7 +2,7 @@
 public class Ex01 {
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
+
 		System.out.println("프로젝트 초안입니다 열심히하세요");
 		System.out.println("팀원1 어려워요");
 		System.out.println("팀장 힘들어요 ㅠㅠ");
@@ -12,11 +12,11 @@ public class Ex01 {
 		System.out.println("나만의 작업 커밋");
 		
 		
-=======
+
 		System.out.println("자 다들 새로운 마음으로 시작합시다");
 		System.out.println("깔끔하고 새로운 코드");
 		System.out.println("by 팀장");
 
->>>>>>> cce23ea3ffc25e22c7e63385661e024a6e0801a7
+
 	}
 }
